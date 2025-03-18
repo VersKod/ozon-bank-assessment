28b163f9499d7bf99bdb1030c10be10a4b0c2370
Kim Andrey <kimanderson5370@gmail.com>
2025-03-18 13:52:34 +0300

    Validation added

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import { updateProgress, controlRotation, hideCircle } from './services.js';
+import { updateProgress, controlRotation, hideCircle, checkNaNValue } from './services.js';
 
 const progressCircle = document.getElementById('circle');
 const valueInput = document.getElementById('value');
@@ -6,7 +6,10 @@ const animateToggle = document.getElementById('animate');
 const hideToggle = document.getElementById('visibility');
 
 valueInput.addEventListener('input', () => updateProgress(valueInput, progressCircle));
+valueInput.addEventListener('keypress', checkNaNValue);
+
 animateToggle.addEventListener('change', () =>
   controlRotation(animateToggle, progressCircle)
 );
+
 hideToggle.addEventListener('change', ()=> hideCircle(hideToggle, progressCircle) )
